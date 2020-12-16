092041e2f1479294582234f6de9d839b191de21a
Stuart <stuart_thirkettle@eventricity.biz>
2020-12-16 12:09:08 +0000

    initial

---
+++ ConsoleAppWithDI/Executor.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+public class Executor
+{
+    private readonly ILogger<Executor> _logger;
+    private readonly IConfiguration _config;
+
+    public Executor(ILogger<Executor> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+    public async Task RunAsync()
+    {
+        _logger.LogInformation("(NOT) RUNNING ASYNC");
+
+    }
+
+    public void Run()
+    {
+        _logger.LogInformation("RUNNING");
+
+    }
+}
---
+++ ConsoleAppWithDI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+
+Log.Logger.Information("Launching");
+
+
+var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+{
+    services.AddTransient<Executor>();
+
+}).UseSerilog().Build();
+
+
+var executor = ActivatorUtilities.CreateInstance<Executor>(host.Services);
+await executor.RunAsync();
+executor.Run();
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+}
