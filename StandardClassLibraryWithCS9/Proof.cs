3b92b85ef119c2b5663ccd9768936385e704d668
Stuart <stuart_thirkettle@eventricity.biz>
2021-01-14 11:31:15 +0000

    added .net standard c# 9 template

---
+++ StandardClassLibraryWithCS9/Proof.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StandardClassLibraryWithCS9
+{
+    /// <summary>
+    /// Demo class to show that csharp 9 is enabled
+    /// init property was introduced with csharp9
+    /// </summary>
+    public class Proof
+    {
+        public int InitProperty { get; init; }
+
+    }
+}
