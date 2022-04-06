9e5b26ff75ba983c15e5ad88919d270dfbd7914d
Stuart <stuart_thirkettle@eventricity.biz>
2022-04-06 18:38:34 +0100

    added dotnet 6 application template

---
+++ ConsoleAppWithDI-dn6/Executor.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Sidereal.Executor
+{
+    public class Executor
+    {
+        private readonly ILogger<Executor> _logger;
+        private readonly IConfiguration _config;
+
+        public Executor(ILogger<Executor> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+        public async Task RunAsync()
+        {
+            _logger.LogInformation("(NOT) RUNNING ASYNC");
+
+        }
+
+        public void Run()
+        {
+            _logger.LogInformation("RUNNING");
+
+        }
+    }
+}
---
+++ ConsoleAppWithDI-dn6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+using Sidereal.Executor;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+
+Log.Logger.Information("Launching");
+
+
+var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+{
+    //services.AddTransient<Executor>();
+
+}).UseSerilog().Build();
+
+
+var executor = ActivatorUtilities.CreateInstance<Executor>(host.Services);
+await executor.RunAsync();
+executor.Run();
+
+Log.Logger.Information("Closing");
+Log.CloseAndFlush();
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+}
